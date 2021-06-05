b9e1e09f8e8fa57d84336713848a1161b5f4ac8b
zora89 <zorawar.purohit@gmail.com>
2021-06-05 08:33:15 +0530

    comments added

--- pet_store.py
+++ pet_store.py
@@ -1,8 +1,13 @@
+#IMPORTING THE NEEDS
+
 from flask import Flask
 from helper import pets
 
+#instance of the flask variable
 app = Flask(__name__)
 
+
+#using decorators, setting .route to homepage with index function
 @app.route('/')
 def index():
   return '''
@@ -15,25 +20,31 @@ def index():
   </ul>
   '''
 
+
+#got new here. Collecting variable paths, establish in previous index("/") page
 @app.route('/animals/<pet_type>')
 def animals(pet_type):
   html = f'<h1> List of {pet_type}: </h1>'
   html += '<ul>'
+
+#enumerate functions returns tuples with counting. Default value to start count can be setup
   for idx, pet in enumerate(pets[pet_type]):
     html += '<li>{link}{namey}</li>'.format(link='<a href="/animals/{pet_type}/{idx}">'.format(pet_type=pet_type, idx=idx),namey='{n}'.format(n=pet['name']))
   html += '</a>'
   html += '</ul>'  
   return html
 
+
+#setting up the deepest page, with new new pet_id. Comes from previous pet_type handling page.
 @app.route('/animals/<pet_type>/<int:pet_id>')
 def pet(pet_type, pet_id):
     pet = pets[pet_type][pet_id]
     return f"""
-  <h1>{pet['name']}</h1>
+  <h1>Name: {pet['name']}</h1>
   <img src={pet['url']} alt='alt text'>
-  <p>{pet['description']}</p>
+  <p><h2>Description: </h2> <h4>{pet['description']}</h4></p>
   <ul>
-  <li>{pet['breed']}</li>
-  <li>{pet['age']}</li>
+  <li> <h3>Breed:</h3> {pet['breed']}</li>
+  <li> <h3>Age: </h3> {pet['age']}</li>
   </ul>
   """
