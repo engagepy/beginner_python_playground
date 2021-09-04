fb52cc9ff4a38d0f26ad456bfef83d29f55c3877
zora89 <zorawar.purohit@gmail.com>
2021-09-05 03:36:48 +0530

    resolved errors

---
+++ creating class and string representation and more.py
@@ -0,0 +1,50 @@
+#creating the class
+
+class Menu:
+#the constructor method using __init__ (self)
+  def __init__(self, name, items, start_time, end_time):
+#must initialise the parameters in a class via self.**
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_time = end_time
+#another application of dunder method, string representation 
+  def __repr__(self):
+#assigning the representative_string required format and values
+    self.representative_string = self.name + " menu is available from {}:00 to {}:00 hours.".format(self.start_time, self.end_time)
+    return self.representative_string
+
+#added new_method, takes items totals bill
+  def calculate_bill(self, purchased_items):
+    self.purchased_items = purchased_items
+    self.total  = 0
+    for i in purchased_items:
+      item = self.items.get(i)
+      self.total += item
+    return self.total
+
+
+#Using the menu class to create the menus
+brunch = Menu('Brunch', {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}, 11, 16)
+
+early_bird = Menu('Early-bird', {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+} , 15, 18)
+
+dinner = Menu('Dinner', {
+  'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}, 17, 23)
+
+kids = Menu('Kids', {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}, 11, 21)
+#testing string representation.
+print(str(brunch.end_time) + " pm", early_bird.end_time, dinner.end_time, kids.end_time)
+
+
+print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
+
+    
+    
---
+++ website_test.py
@@ -0,0 +1,63 @@
+import requests
+from collections import namedtuple
+import time
+import progressbar
+  
+widgets = [' [',
+         progressbar.Timer(format= 'elapsed time: %(elapsed)s'),
+         '] ',
+           progressbar.Bar('*'),' (',
+           progressbar.ETA(), ') ',
+          ]
+  
+bar = progressbar.ProgressBar(max_value=20, 
+                              widgets=widgets).start()
+  
+for i in range(20):
+    time.sleep(0.1)
+    bar.update(i)
+
+
+
+WebsiteStatus = namedtuple('WebsiteStatus', ['status_code', 'reason'])
+names = ['astratechz', 'multitechnoservices', 'northlandindia', 'nivaunited', ]
+namesin = [ 'goandamans', 'multitechno','singhotels']
+
+def get_status(site):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    try:
+        response = requests.head(site, timeout=5, allow_redirects=True, headers=headers)
+        #response = requests.head(site, timeout=5)
+        status_code = response.status_code
+        reason = response.reason
+    except requests.exceptions.ConnectionError:
+        status_code = '000'
+        reason = 'ConnectionError'
+    website_status = WebsiteStatus(status_code, reason)
+    return website_status
+
+print("TESTING FOR .COM WEBSITES NOW>>>> \n")
+for name in names:
+    for i in range(20):
+        time.sleep(0.01)
+        bar.update(i)
+
+
+    
+    site = 'http://{}.com'.format(name)
+    website_status = get_status(site)
+    print("{0:30} {1:10} {2:10}".format(site, website_status.status_code, website_status.reason))
+
+print("\n TESTING FOR .IN WEBSITES NOW>>>> \n") 
+          
+for namein in namesin:
+    for i in range(20):
+        time.sleep(0.01)
+        bar.update(i)
+
+
+
+    site = 'http://{}.in'.format(namein)
+    website_status = get_status(site)
+    print("{0:30} {1:10} {2:10}".format(site, website_status.status_code, website_status.reason))
+    
