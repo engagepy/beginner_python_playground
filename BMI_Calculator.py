03c51c48b9b5a0ee605b12250b1e5261e4162d63
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-01-24 23:13:23 +0530

    final
    Underweight condition added.
    Comments added

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,9 +1,10 @@
-
+#Purpose : To calculate BMI
+# Step 1: Take input , Step 2: To check if height is valid , Step 3:
 def take_input():
     try:
 
         print (" ")
-        name = input("Your name:   ")
+        name = input("Your name: ")
         print (" ")
         height = float(input("Your Height in meters, eg 1.5 = ")) 
         print (" ")
@@ -14,7 +15,8 @@ def take_input():
         
 
 def height_unit_check(height):
-    if height >= 2.2:
+    # 2.72 m is the recorded height for the tallest human ever.
+    if height >= 2.72:
         print ("")
         print ("My ego ain't that tall, are you sure you are. Try again!")
     elif height == 0 :
@@ -34,24 +36,19 @@ def BMI_CAL(name,height,weight):
     
     if float_x == 25.00:
         print ("You are on the fine line leaning towards the Dark Side, Start working out!")
-    elif float_x >= 25.00 :
-        print ("If you care about your smile and want to keep it. Start working out today!")
+    elif float_x > 25.00 :
+        print ("You have joined the Dark Side.If you care about your smile and want to keep it. Start working out today!")
+    elif float_x <= 18.5 :
+        print ("You are underweight my friend!")
     else: 
         print ("You are fit and fine smile away!")
-
-
 
 while True: 
 	name, height, weight = take_input()
 	height_unit_check(height)
 
 
-    
-
-
-
-
 #try:
 #    height_unit_check
 #except unit_input_error:
-#    print ("My ego ain't that tall, are you sure you are. Try again!")    
+#    print ("My ego ain't that tall, are you sure you are. Try again!")
