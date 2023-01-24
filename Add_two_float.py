2e1e4f69bf019b3e1a8172cabe5ba8925d746543
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-01-24 22:36:16 +0530

    final
    Tested delay and added some comments

--- Add_two_float.py
+++ Add_two_float.py
@@ -1,9 +1,14 @@
-# test code
-from time import sleep
+# Purpose : 1) To add two float numbers. 2) Delay the output by 2 seconds
+
+import time
 a=float(input("Enter a Number: "))
 b=float(input("Enter a Number: "))
-print("This is the sum total of numbers enters!", a+b )
 
-sleep(2)
+# Checking time before and after execution of sum.
+
+print ("After Input : %s" %time.ctime())
+time.sleep(2)
+print("This is the sum total of numbers enters!", a+b )
+print ("After Execution : %s" %time.ctime())
 
 
