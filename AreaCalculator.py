9ee6f807d4859affdfcb172c3dbf35cd582d4e4a
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-01-24 22:46:59 +0530

    final
    Added Delay to give an animated view.
    Grammatical Errors corrected.

--- AreaCalculator.py
+++ AreaCalculator.py
@@ -1,18 +1,23 @@
-# create a shape area calculator. Input step 1, calculate step 2, output step 3
-print ("Shape Calculator starting up....")
-name = input("Enter C for circle or T for Traingle: ")
-if name == "c":
+# Purpose :Create a shape area calculator. 
+# Step 1: Input , Step 2: Calculate , Step 3: Output
+
+import time
+print ("Shape Area Calculator starting up....")
+# Delay of 1 second added .
+time.sleep(1)
+name = input("Enter C for Circle or T for Triangle: ")
+if name == "c" or "C":
   radius = float(input("Enter the radius :"))
   area = 3.14159*(radius**2)
-  print ("Area is equal to " + str(area) + " .")
-elif name == "t":
+  print ("Area of the circle is " + str(area) + " .")
+elif name == "t" or "T":
   base = float(input("Enter the base of the triangle :"))
   height = float(input("Enter the height of the triangle :"))
   area = 0.5*(base*height)
-  print (" The area of the Triangle is " + str(area) + " ")
+  print (" Area of the Triangle is " + str(area) + " ")
 else:
   print ("Enter a valid shape. Try again.")
-print ("Program Terminating")
+print ("Program Terminated.")
 
 
 
